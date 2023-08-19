3dc57b597bf4ac8792c95f901cd5b5729fa3f319
Akhmadjon Jurayev <shunchakiadres@gmail.com>
2023-08-19 16:02:32 +0500

    Initial

---
+++ Ngnix_Proxy/HostAddress.cs
@@ -0,0 +1,11 @@
+﻿namespace Ngnix_Proxy
+{
+    public class HostAddress
+    {
+        public string Host { get; set; }
+
+        public string Address { get; set; }
+
+        public int Port { get; set; }
+    }
+}
